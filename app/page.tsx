358aebabf87482b5b7300d3a40161884a56b641a
Tufan.Butuner <Tufan.Butuner@transformuk.com>
2023-10-05 22:33:21 +0100

    add image to posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { getPosts } from "@/sanity/schemas/sanity-utils";
+import Image from "next/image";
 
 export default async function Home() {
   const posts = await getPosts();
@@ -7,9 +8,10 @@ export default async function Home() {
 
   return (
     <div>
-      posts go here:
+      Latest posts:
       {posts.map((post) => (
         <div key={post._id} style={{ margin: "2rem" }}>
+          <Image src={post.image} width={200} height={200} alt="" />
           {post.name}
           <p>{post.author}</p>
         </div>
