17f92405d1027ad6407b6a29d23e913285b50ab9
Tufan.Butuner <Tufan.Butuner@transformuk.com>
2023-10-06 13:28:05 +0100

    navbar logo links to homepage

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
-    <div className="navbar">
-      <p className="navbar-header">VIVEZA</p>
-    </div>
+    <Link href={"/"}>
+      <div className="navbar">
+        <p className="navbar-header">VIVEZA</p>
+      </div>
+    </Link>
   );
 }
