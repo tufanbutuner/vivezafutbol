63a2322b44938f4124c64812204eb6863f33173e
Tufan.Butuner <Tufan.Butuner@transformuk.com>
2023-10-11 12:24:43 +0100

    add G Tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
 import LatestArticles from "@/components/LatestArticles";
+import Script from "next/script";
 
 export default async function Home() {
   return (
     <div className="container">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-27DMNSPFPY" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-27DMNSPFPY');
+        `}
+      </Script>
+
       <h1 className="posts-header">LATEST ARTICLES</h1>
       <LatestArticles />
     </div>
--- app/posts/[post]/page.tsx
+++ app/posts/[post]/page.tsx
@@ -1,6 +1,7 @@
 import { getPost } from "@/sanity/schemas/sanity-utils";
 import { PortableText } from "@portabletext/react";
 import Image from "next/image";
+import Script from "next/script";
 
 type Props = {
   params: { post: string };
@@ -11,23 +12,35 @@ export default async function Post({ params }: Props) {
   const post = await getPost(slug);
 
   return (
-    <div className="post-container">
-      <h1>{post.name}</h1>
+    <>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-27DMNSPFPY" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-27DMNSPFPY');
+        `}
+      </Script>
+      <div className="post-container">
+        <h1>{post.name}</h1>
 
-      <div className="post-image-container">
-        <Image
-          src={post.image}
-          alt={post.name}
-          fill
-          sizes="(max-width: 768px) 100vh, 700px"
-          objectFit="cover"
-          priority
-        />
-      </div>
+        <div className="post-image-container">
+          <Image
+            src={post.image}
+            alt={post.name}
+            fill
+            sizes="(max-width: 768px) 100vh, 700px"
+            objectFit="cover"
+            priority
+          />
+        </div>
 
-      <div>
-        <PortableText value={post.content} />
+        <div>
+          <PortableText value={post.content} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
