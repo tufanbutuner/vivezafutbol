3ab467c455029f0a33ce8f96fb435d95140dd64d
Tufan.Butuner <Tufan.Butuner@transformuk.com>
2023-10-19 21:53:56 +0100

    Add social to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,17 @@
-export default function Navbar() {
+import Link from "next/link";
+import { FaInstagram, FaXTwitter } from "react-icons/fa6";
+
+export default function Footer() {
   return (
     <div className="footer">
+      <div className="footer-icons">
+        <Link href="/" target="_blank">
+          <FaXTwitter size={24} />
+        </Link>
+        <Link href="https://www.instagram.com/vivezafutbol" target="_blank">
+          <FaInstagram size={24} />
+        </Link>
+      </div>
       <p className="">&copy; Viveza Futbol</p>
     </div>
   );
